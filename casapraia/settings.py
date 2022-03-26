843dc4f11405aaf8521b10a9a4ee3a1af06c695c
boni <jbonifacio07@gmail.com>
2022-03-26 17:21:26 -0300

    QUARTO commit

--- casapraia/settings.py
+++ casapraia/settings.py
@@ -160,7 +160,7 @@ STATICFILES_DIRS = [
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'casapraia/static',
 ]
 
 MEDIA_ROOT = BASE_DIR / 'media'
