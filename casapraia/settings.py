575bdf55e3b491b5769f6c9fe105bc232a9087a3
boni <jbonifacio07@gmail.com>
2022-03-29 17:34:16 -0300

    session_cookie_age

--- casapraia/settings.py
+++ casapraia/settings.py
@@ -177,3 +177,6 @@ LOGIN_REDIRECT_URL = 'index'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_COOKIE_AGE = 60 * 60 * 1
+
