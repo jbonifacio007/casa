fd825f9b2a3cbea92dfcd8356b5057dbc9e909b2
boni <jbonifacio07@gmail.com>
2022-03-29 18:00:13 -0300

    validacao da inicial reserva

--- reservas/models.py
+++ reservas/models.py
@@ -2,12 +2,18 @@ from django.db import models
 from cadastros.models import Base,Membro
 from django.utils import timezone
 # Create your models here.
+from django.core.exceptions import ValidationError
 
 
 class Reserva(Base):
     membro = models.ForeignKey(Membro, on_delete=models.RESTRICT, verbose_name='Membro' )
     data_inicio = models.DateField(verbose_name='Data Início',auto_now=False, auto_now_add=False)
     data_final = models.DateField(verbose_name='Data Final',auto_now=False, auto_now_add=False)
+
+    def clean(self):
+
+        if self.data_inicio > self.data_final :
+            raise ValidationError({'data_inicio': ('Data Início não pode ser maior que a final!')})
 
 
     class Meta:
